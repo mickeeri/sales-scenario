b9ce0c6a7ba5776c45105fcd0b12ef0dd6b71fd0
fh222dt <fh222dt@student.lnu.se>
2016-02-10 15:54:09 +0100

    JPlayer utan custom style

--- sales-scenario/resources/views/layouts/app.blade.php
+++ sales-scenario/resources/views/layouts/app.blade.php
@@ -5,6 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+    <!-- Styles -->
+    <link type="text/css" href="/css/main.css" rel="stylesheet" />
+
+    @yield('css')
     <title>Sales Scenario</title>
 </head>
 <body>
@@ -26,6 +35,7 @@
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        @yield('js')
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
--- sales-scenario/resources/views/player.blade.php
+++ sales-scenario/resources/views/player.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 
+@section('css')
+    <!-- Style for circle player -->
+    <link type="text/css" href="/css/circle.player.css" rel="stylesheet" />
+@endsection
+
+@section('js')
+    <!-- JPlayer -->
+    <script type="text/javascript" src="/js/jquery.transform2d.js"></script>
+    <script type="text/javascript" src="/js/jquery.grab.js"></script>
+    <script type="text/javascript" src="/js/mod.csstransforms.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.jplayer.min.js"></script>
+    <script type="text/javascript" src="/js/circle.player.js"></script>
+    <script type="text/javascript" src="/js/audioplayer.js"></script>
+@endsection
+
 @section('content')
+    <!-- The jPlayer div must not be hidden. Keep it at the root of the body element to avoid any such problems. -->
+    <div id="jquery_jplayer_1" class="cp-jplayer"></div>
     <div id="slider">
         <h2>What's new</h2>
         <div class="slide">
@@ -15,4 +32,25 @@
             Your browser does not support the audio element.
         </audio>
     </div>
+
+
+
+        <!-- The container for the interface can go where you want to display it. Show and hide it as you need. -->
+        <div id="cp_container_1" class="cp-container">
+            <div class="cp-buffer-holder"> <!-- .cp-gt50 only needed when buffer is > than 50% -->
+                <div class="cp-buffer-1"></div>
+                <div class="cp-buffer-2"></div>
+            </div>
+            <div class="cp-progress-holder"> <!-- .cp-gt50 only needed when progress is > than 50% -->
+                <div class="cp-progress-1"></div>
+                <div class="cp-progress-2"></div>
+            </div>
+            <div class="cp-circle-control"></div>
+            <ul class="cp-controls">
+                <li><a class="cp-play" tabindex="1">play</a></li>
+                <li><a class="cp-pause" style="display:none;" tabindex="1">pause</a></li> <!-- Needs the inline style here, or jQuery.show() uses display:inline instead of display:block -->
+            </ul>
+        </div>
+
+
 @endsection
