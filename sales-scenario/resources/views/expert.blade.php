582d7987926f59951cde39ca1bc2597d58c361b1
Matilda laptop <ingman.m@gmail.com>
2016-02-16 13:30:35 +0100

    Link to experts website

--- sales-scenario/resources/views/expert.blade.php
+++ sales-scenario/resources/views/expert.blade.php
@@ -7,7 +7,7 @@
 @section('content')
         <h1>{{ $expert->first_name }} {{ $expert->last_name }} </h1>
         <p>{{ $expert->info }}</p>
-        <p><em> {{ $expert->website }}</em></p>
+        <p><em><a href=" {{ $expert->website }}"> {{ $expert->website }}</a></em></p>
         <!--Insert photo somewhere here. Should we show tags here also? -->
         @foreach($expert->podcasts as $podcast)
             <a href="/player/{{ $expert->id }}/{{ $podcast->id }}">{{ $podcast->title }}</a><br>
