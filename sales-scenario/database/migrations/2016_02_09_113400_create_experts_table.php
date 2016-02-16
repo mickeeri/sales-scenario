8771f2a035ec5e856fcba0e9eefa421b4b694cc0
Roy Nilsson <rn222cx@student.lnu.se>
2016-02-16 10:10:42 +0100

    remove photo column from expert and created new migration with photo

--- sales-scenario/database/migrations/2016_02_09_113400_create_experts_table.php
+++ sales-scenario/database/migrations/2016_02_09_113400_create_experts_table.php
@@ -18,7 +18,6 @@ class CreateExpertsTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('website');
-            $table->string('photo');
             $table->text('info');
             $table->timestamps();
 
---
+++ sales-scenario/database/migrations/2016_02_16_085355_add_photo_to_experts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPhotoToExpertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('experts', function (Blueprint $table) {
+            $table->string('photo')->after('website');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('experts', function (Blueprint $table) {
+
+        });
+    }
+}
