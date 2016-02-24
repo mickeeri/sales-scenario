92d2ad0c30d475ef839d1b21192a36a901f69e62
Matilda laptop <ingman.m@gmail.com>
2016-02-24 11:13:15 +0100

    Soft delete added to user and expert

--- sales-scenario/app/Expert.php
+++ sales-scenario/app/Expert.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Expert extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'experts';
     /**
      * The attributes that are mass assignable.
@@ -14,6 +17,15 @@ class Expert extends Model
      */
     protected $fillable = [
         'first_name', 'last_name', 'website', 'info'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates
+     *
+     * @var array
+     */
+    protected $dates = [
+        'deleted_at'
     ];
 
     /**
