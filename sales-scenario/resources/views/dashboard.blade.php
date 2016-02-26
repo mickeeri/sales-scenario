5d2930cbdfec5df3104800891f15297fe7cb7a12
ehamrin <eh222ve@student.lnu.se>
2016-02-26 10:08:17 +0100

    jQuery was loaded twice

--- sales-scenario/resources/views/dashboard.blade.php
+++ sales-scenario/resources/views/dashboard.blade.php
@@ -39,7 +39,6 @@
 
 @section('js')
         <!-- Script for slider-->
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="/slick/slick.min.js"></script>
 <script>
