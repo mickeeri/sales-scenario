9d0a6c8b9b3890d0221ede5d5694d51e2e637d94
fh222dt <fh222dt@student.lnu.se>
2016-02-29 14:15:39 +0100

    dummy pic on expert in dashboard-, expert- & player views

--- sales-scenario/resources/views/expert.blade.php
+++ sales-scenario/resources/views/expert.blade.php
@@ -14,7 +14,7 @@
                 <h1>{{ $expert->first_name }} {{ $expert->last_name }} </h1>
                 <p>{{ $expert->info }}</p>
                 <p><em><a href=" {{ $expert->website }}"> {{ $expert->website }}</a></em></p>
-                <img src="/expert_photo/{{ $expert->photo }}" alt="Profile picture of {{ $expert->first_name }} {{ $expert->last_name }}"/>
+                @include('partials.expert_img', array('src' =>$expert->photo, 'class' =>'profile-img', 'first' =>$expert->first_name, 'last' =>$expert->last_name))
         </div>
         <!--Insert photo somewhere here. Should we show tags here also? -->
         @if(count($expert->podcasts))
