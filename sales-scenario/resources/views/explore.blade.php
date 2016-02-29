6a7580af76887bca57ccd2734e40419276cdaba7
Roy Nilsson <rn222cx@student.lnu.se>
2016-02-29 12:18:49 +0100

    view didn't push last commit. try again

--- sales-scenario/resources/views/explore.blade.php
+++ sales-scenario/resources/views/explore.blade.php
@@ -2,35 +2,28 @@
 
         <!-- Only run js if tag value is present -->
 @if ($tag)
-@section('js')
-    <script>
-        $(function () {
-            // Get the tag id.
-            var tag = {{ $tag }} -1;
-            // Get the value from input
-            var value = $("ul#filter-tags input:nth(" + tag + ")").val();
-            // Remove all tags except the one with present id
-            contain = $('#filter-tags').map(function () {
-                return ':contains("' + value + '")';
-            }).get();
-            $('#expert-list .expert:not(' + contain + ')').hide();
-            // Uncheck checkboxes except the present one
-            $(':checkbox').attr('checked', false)[tag].checked = true;
-        });
-
-        $('.filter-popup').toggle();
-
-        //TODO Just to test toogle sort list...
-        $(function(){
-            $('#hideshow').on('click', function(event) {
-                $('.filter-popup').toggle('show');
+    @section('js')
+        <script>
+            $(function () {
+                // Get the tag id.
+                var tag = {{ $tag }} -1;
+                // Get the value from input
+                var value = $("ul#filter-tags input:nth(" + tag + ")").val();
+                // Remove all tags except the one with present id
+                contain = $('#filter-tags').map(function () {
+                    return ':contains("' + value + '")';
+                }).get();
+                $('#expert-list .expert:not(' + contain + ')').hide();
+                // Uncheck checkboxes except the present one
+                $(':checkbox').attr('checked', false)[tag].checked = true;
             });
-        });
-
-
-    </script>
-@endsection
+        </script>
+    @endsection
 @endif
+
+@section('js')
+    <script type="text/javascript" src="/js/tags.toggle.js"></script>
+@endsection
 
 @section('content')
     @if (session('status'))
@@ -53,11 +46,10 @@
                     </li>
                 @endforeach
                 <li>
-                    <input type="checkbox" value="Show All" id="check_all" checked/> Show All
+                    <input type="checkbox" name="show-all" id="check_all" checked/> Show All
                     <label for="check_all"><span></span></label>
                 </li>
             </ul>
-            <a href="#">Close or Save...</a>
         </div>
 
         <div id="expert-list">
