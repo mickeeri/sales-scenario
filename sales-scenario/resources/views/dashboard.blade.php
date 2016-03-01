397b1e48e28d00510430c862b31ea5e1f984c204
sj222mk <sj222mk@student.lnu.se>
2016-03-01 10:17:35 +0100

    cdn ist

--- sales-scenario/resources/views/dashboard.blade.php
+++ sales-scenario/resources/views/dashboard.blade.php
@@ -1,9 +1,28 @@
 @extends('layouts.app')
 @section('css')
         <!-- Styles for slider-->
-<link rel="stylesheet" type="text/css" href="/slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="/slick/slick-theme.css"/>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css"/>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick-theme.css"/>
 @endsection
+
+@section('js')
+        <!-- Script for slider-->
+<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js"></script>
+<!--    <script type="text/javascript" src="/slick/slick.min.js"></script> -->
+<script>
+    $(document).ready(function(){
+        $('.slider').slick({
+            accessibility:true,
+            autoplay: false,
+            autoplaySpeed: 2000,
+            dots: true,
+            mobileFirst: true
+
+        });
+    });
+</script>
+@endsection
+
 @section('content')
     <div id="slider-wrapper">
         <h2 class="center upper">What's new</h2>
@@ -37,21 +56,5 @@
     </ul>
 @endsection
 
-@section('js')
-        <!-- Script for slider-->
-<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="/slick/slick.min.js"></script>
-<script>
-    $(document).ready(function(){
-        $('.slider').slick({
-            accessibility:true,
-            autoplay: false,
-            autoplaySpeed: 2000,
-            dots: true,
-            mobileFirst: true
 
-        });
-    });
-</script>
-@endsection
 
