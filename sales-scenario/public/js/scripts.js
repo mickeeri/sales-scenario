a26b03ba7fa550db6fa8d525a5c49db7025ffd28
fh222dt <fh222dt@student.lnu.se>
2016-03-02 11:22:39 +0100

    popup script fixed, removed unused files

--- sales-scenario/public/js/scripts.js
+++ sales-scenario/public/js/scripts.js
@@ -2,3 +2,14 @@
 $('#menu_open').click(function(e){
     $('body').toggleClass('active');
 });
+
+/**
+ * Toggle expert info
+ */
+
+$(document).ready(function() {
+    $('.podcast-author').click(function () {
+        $('#expert_info').toggle();
+        $(this).toggleClass('active');
+    });
+});
