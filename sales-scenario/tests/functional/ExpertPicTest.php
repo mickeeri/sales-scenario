deb41cab79853e48aa8bc804757fbbeb99c5e975
fh222dt <fh222dt@student.lnu.se>
2016-03-03 12:10:51 +0100

    removed DRY

--- sales-scenario/tests/functional/ExpertPicTest.php
+++ sales-scenario/tests/functional/ExpertPicTest.php
@@ -10,6 +10,20 @@ class ExpertPicTest extends TestCase
     use DatabaseTransactions;
 
     /**
+     * Test expert img
+     *
+     */
+    private function expert_img_is_showing($expert, $podcast, $imgUrl)
+    {
+        //check img in expert-view
+        $this->visit('expert/' . $expert->id)
+            ->see($imgUrl)
+            //check  img in player view
+            ->visit('/player/'.$expert->id.'/'.$podcast->id)
+            ->see($imgUrl);
+    }
+
+    /**
      * Test if expert has img, it is showing
      *
      */
@@ -17,13 +31,9 @@ class ExpertPicTest extends TestCase
     {
         $expert = \App\Expert::whereNotNull('photo')->first();              //Get the 1st expert with photo in db
         $podcast = \App\Podcast::where('expert_id', $expert->id)->first();  //Get 1 podcast from the expert
+        $imgUrl = $expert->photo;
 
-        //check img in expert-view
-        $this->visit('expert/' . $expert->id)
-             ->see('/expert_photo/'.$expert->photo)
-            //check img in player view
-            ->visit('/player/'.$expert->id.'/'.$podcast->id)
-            ->see('/expert_photo/'.$expert->photo);
+        $this->expert_img_is_showing($expert, $podcast, $imgUrl);
     }
 
     /**
@@ -34,13 +44,11 @@ class ExpertPicTest extends TestCase
     {
         $expert = \App\Expert::where('photo', '')->first();                 //Get the 1st expert WITHOUT photo in db
         $podcast = \App\Podcast::where('expert_id', $expert->id)->first();  //Get 1 podcast from the expert
+        $imgUrl = 'expert_photo/blank-profile-picture.png';
 
-        //check dummy img in expert-view
-        $this->visit('expert/' . $expert->id)
-            ->see('expert_photo/blank-profile-picture.png')
-            //check dummy img in player view
-            ->visit('/player/'.$expert->id.'/'.$podcast->id)
-            ->see('expert_photo/blank-profile-picture.png');
+        $this->expert_img_is_showing($expert, $podcast, $imgUrl);
     }
+
+
 
 }
