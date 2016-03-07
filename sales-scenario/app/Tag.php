aadd4f6be89f9a3c25148f502d1479111d289330
fh222dt <fh222dt@student.lnu.se>
2016-03-07 18:19:56 +0100

    cleaned up model from unused code

--- sales-scenario/app/Tag.php
+++ sales-scenario/app/Tag.php
@@ -6,21 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    /*public static function tagLocation()
-    {
-        return public_path().'/audio/s/';
-    }*/
 
     protected $table = 'tags';
 
     protected $fillable = [
         'name'
     ];
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    /*public function tags()
-    {
-        return $this->belongsToMany('App\Expert')->withTimestamps();
-    }*/
 }
