1874d15a60f818ebaf042fa7728526a28f16a77b
fh222dt <fh222dt@student.lnu.se>
2016-03-07 11:22:51 +0100

    preparing controller for crud tags

--- sales-scenario/app/Tag.php
+++ sales-scenario/app/Tag.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    /*public static function tagLocation()
+    {
+        return public_path().'/audio/s/';
+    }*/
+
+    protected $table = 'tags';
+
+    protected $fillable = [
+        'name'
+    ];
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
