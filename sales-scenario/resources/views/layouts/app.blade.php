42dd7ab2e29725a426cd581862940949f2b9f9cb
fh222dt <fh222dt@student.lnu.se>
2016-03-08 11:46:15 +0100

    smaller logo img for top menu

--- sales-scenario/resources/views/layouts/app.blade.php
+++ sales-scenario/resources/views/layouts/app.blade.php
@@ -37,7 +37,9 @@
     @if (Auth::check())
         <div id="header" class="orange-nav">
             <div id="menu_open"></div>
-            @include('unauthorized.logo')
+            <a href="/" id="logo">
+                <img src="/img/logo_small.png"/>
+            </a>
             <nav class="wrapper">
                 <ul id="top">
                     <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
