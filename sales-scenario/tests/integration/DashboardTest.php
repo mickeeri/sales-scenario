e12f809573f3c89db94298a741ab2fc6895952d6
Mikael Eriksson <me222wm@student.lnu.se>
2016-03-08 16:55:05 +0100

    Fixed dashboard-test and added DatabaseMigration to TestCase.

--- sales-scenario/tests/integration/DashboardTest.php
+++ sales-scenario/tests/integration/DashboardTest.php
@@ -6,14 +6,23 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class DashboardTest extends TestCase
 {
+    use DatabaseTransactions;
+
     public function test_headings_and_links_in_dashboard_view()
     {
         $this->login();
 
+        // If no experts or podcasts is in database.
         $this->visit('dashboard')
             ->see('Most Contributing')
             ->see('Explore Topics')
             ->seeLink('View more')
+            ->dontSeeLink('Play');
+
+        $this->getExpertWithPodcast();
+
+        // Only show play if expert with podcast exists.
+        $this->visit('dashboard')
             ->seeLink('Play');
     }
 }
