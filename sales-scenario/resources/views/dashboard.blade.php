18570fc0c747fc47d1eea6fc4202f86c97890a7a
Matilda laptop <ingman.m@gmail.com>
2016-03-08 13:47:43 +0100

    Resolved merging issue

--- sales-scenario/resources/views/dashboard.blade.php
+++ sales-scenario/resources/views/dashboard.blade.php
@@ -6,21 +6,10 @@
         @include('partials.slider')
     </div>
     <h2 class="center">Most Contributing</h2>
-
-    <ul class="explore-list">
-        @each('partials.expert_listing', $experts, 'expert')
-    </ul>
+    @include('partials.most_contribution_expert')
     <a href="/explore" class="read-more">View more</a>
 
     <h2 class="center">Explore topics</h2>
-
-    <ul class="tag-list explore-list">
-        @foreach($tags as $tag)
-            <li><a href="/explore/{{ $tag->slug }}"><span>{{ $tag->name }}</span></a></li>
-        @endforeach
-    </ul>
+    @include('partials.explore_topics')
 
 @endsection
-
-
-
