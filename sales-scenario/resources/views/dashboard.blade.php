d23efa6488635a0fa3b422b12f65359c59dad79c
Matilda laptop <ingman.m@gmail.com>
2016-03-08 09:59:47 +0100

    Most contribution moved to partial view

--- sales-scenario/resources/views/dashboard.blade.php
+++ sales-scenario/resources/views/dashboard.blade.php
@@ -1,15 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+
     <div id="slider_wrapper">
         <h2 class="center upper">What's new</h2>
         @include('partials.slider')
     </div>
     <h2 class="center">Most Contributing</h2>
+    @include('partials.most_contribution_expert')
 
-    <ul class="explore-list">
-        @each('partials.expert_listing', $experts, 'expert')
-    </ul>
     <a href="/explore" class="read-more">View more</a>
 
     <h2 class="center">Explore topics</h2>
