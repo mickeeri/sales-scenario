a83fc4047924784319d3847ae4c0cb3b9f96ef87
Andréas Anemyr <aa223ig@student.lnu.se>
2016-03-11 22:07:33 +0100

    issue#173 History not blank...
    p element medföreslagen text:
    You have not yet listened to any podcasts

--- sales-scenario/resources/views/history.blade.php
+++ sales-scenario/resources/views/history.blade.php
@@ -27,15 +27,20 @@
     </div>
     <div class="wrapper no-padding">
         <ul class="explore-list">
-            @foreach($podcasts as $podcast)
-                <li class="expert">
-                    <a href="/player/{{ $podcast->expert->slug }}/{{ $podcast->slug }}">
+            @if(count($podcasts) != 0)
+                @foreach($podcasts as $podcast)
+                    <li class="expert">
+                        <a href="/player/{{ $podcast->expert->slug }}/{{ $podcast->slug }}">
+                            <span class="title">{{ $podcast->title }}</span><br/>
+                            <span class="podcast-date">{{ $podcast->expert->full_name }}<br/>Played on {{ $podcast->pivot->created_at->format('F d Y') }}</span>
+                        </a>
+                    </li>
+                @endforeach
+            @else
+                <p>You have not yet listened to any podcasts</p>
+            @endif
 
-                        <span class="title">{{ $podcast->title }}</span><br/>
-                        <span class="podcast-date">{{ $podcast->expert->full_name }}<br/>Played on {{ $podcast->pivot->created_at->format('F d Y') }}</span>
-                    </a>
-                </li>
-            @endforeach
+
         </ul>
     </div>
 @endsection
